71922b5c2d1ff8a4fe0e660fc14e788952e036da
omegamit6zeichen <markus.adamek@gmail.com>
2016-03-27 19:24:03 +0200

    initial commit

---
+++ prinject/DependencyHandlerTests/HandlerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using prinject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyHandlerTests
+{
+    [TestFixture]
+    public class HandlerTests
+    {
+
+        [Test]
+        void SimpleDependencyTest()
+        {
+            DependencyHandler.Instance.InstallDependency(new object());
+        }
+    }
+}
---
+++ prinject/prinject/DependencyHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prinject
+{
+    public class DependencyHandler
+    {
+        private static DependencyHandler _handler = null;
+
+        public static DependencyHandler Instance
+        {
+            get { return _handler ?? new DependencyHandler(); }
+        }
+
+        public void InstallDependency(Type t, object obj)
+        {
+        }
+
+        public void InstallDependency<T>(T obj)
+        {
+            InstallDependency(typeof(T), obj);
+        }
+
+    }
+}
