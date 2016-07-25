a299fe34d91b3a7de86a81bd11f12ff4d902571b
omegamit6zeichen <markus.adamek@gmail.com>
2016-07-25 21:00:50 +0200

    added missing file

---
+++ prinject/DependencyHandlerTests/Mockings/MockingDepCheck.cs
@@ -0,0 +1,42 @@
+﻿using prinject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyHandlerTests.Mockings
+{
+    public class MockingDepCheck
+    {
+        [Resolve(DependendyFlag.Essential )]
+        public string Essential
+        {
+            get;
+            set;
+        } = null;
+
+
+        [Resolve(DependendyFlag.Optional)]
+        public int? Optional
+        {
+            get;
+            set;
+        } = null;
+
+
+        [Resolve(DependendyFlag.Shared,0)]
+        public double? Shared1
+        {
+            get;
+            set;
+        }
+
+        [Resolve(DependendyFlag.Shared, 0)]
+        public object Shared2
+        {
+            get;
+            set;
+        }
+    }
+}
